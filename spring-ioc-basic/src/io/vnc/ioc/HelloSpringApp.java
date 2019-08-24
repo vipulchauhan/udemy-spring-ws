c2340b69ed3b5a3f00afabab7043ef72f04419ba
Vipul Chauhan <08bce016@gmail.com>
2019-08-24 19:11:05 +0530

    added logging cababilities

--- spring-ioc-basic/src/io/vnc/ioc/HelloSpringApp.java
+++ spring-ioc-basic/src/io/vnc/ioc/HelloSpringApp.java
@@ -10,10 +10,16 @@ public class HelloSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("trackCoach", Coach.class);
+		Coach trackCoach = context.getBean("trackCoach", Coach.class);
 
 		// call methods on the bean
-		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(trackCoach.getDailyWorkout());
+
+		// retrieve bean from spring container
+		Coach baseballCoach = context.getBean("baseballCoach", Coach.class);
+
+		// call methods on the bean
+		System.out.println(baseballCoach.getDailyWorkout());
 
 		// close the context
 		context.close();
