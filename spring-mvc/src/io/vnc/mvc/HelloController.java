0e43f1784b7dbec93f8342821ec3dd06386e267c
Vipul Chauhan <08bce016@gmail.com>
2019-09-11 23:17:42 +0530

    spring-mvc basic project

---
+++ spring-mvc/src/io/vnc/mvc/HelloController.java
@@ -0,0 +1,13 @@
+package io.vnc.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+}
